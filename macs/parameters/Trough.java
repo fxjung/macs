b4f315b4d1b0ee46b096510f900bee05d4b817fd
simon martin <simon_p_martin@btinternet.com>
2016-04-20 10:29:04 +0100

    added copyright statements

--- macs/parameters/Trough.java
+++ macs/parameters/Trough.java
@@ -1,24 +1,17 @@
-/**
- * MACS - Multi-Agent Cooperative Search is a framework to develop
- * cooperating agents using different Metaheuristics
- * Copyright (C) 2013 University of Stirling
- *
- * GNU Lesser General Public License
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation,
- * version 2.1 of the License.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA  02111-1307, USA.
+/** 
+ * MACS - Multi-Agent Cooperative Search is a framework to develop cooperating agents using 
+ * different Metaheuristics Copyright (C) 2016 Simon Martin. This file is part of MACS. 
+ * 
+ * MACS is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * MACS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License along with MACS. 
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 package macs.parameters;
--- macs/uoc/cvrp/Node.java
+++ macs/uoc/cvrp/Node.java
@@ -1,3 +1,18 @@
+/** 
+ * MACS - Multi-Agent Cooperative Search is a framework to develop cooperating agents using 
+ * different Metaheuristics Copyright (C) 2016 Simon Martin, Angel Alejandro Juan Perez. This file is part of MACS. 
+ * 
+ * MACS is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * MACS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License along with MACS. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 package macs.uoc.cvrp;
 
 import macs.ontologies.entities.problems.NodeData;
